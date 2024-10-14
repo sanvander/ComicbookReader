ef414b6ff1ff1fd11613764e4582114238b75f95
sander van der werff <sander-werff@hotmail.com>
2024-10-14 17:00:20 +0200

    ADD first progress of making the UI

---
+++ src/main/java/org/ComicReaderSander/View/Homescreen.java
@@ -0,0 +1,16 @@
+package org.ComicReaderSander.View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Homescreen extends JPanel {
+
+    private JPanel Homescreen;
+
+    public Homescreen() {
+        setBackground(Color.BLACK);
+
+    }
+}
--- src/main/java/org/ComicReaderSander/View/SimpleComicUI.java
+++ src/main/java/org/ComicReaderSander/View/SimpleComicUI.java
@@ -1,11 +1,33 @@
 package org.ComicReaderSander.View;
+import javax.swing.*;
+import java.awt.*;
 
-public class SimpleComicUI implements IComicReaderUI {
 
+public class SimpleComicUI extends JFrame implements IComicReaderUI {
 	private String state;
 
-	public void showHomescreen() {
+	public SimpleComicUI(){
+		//Define title and what to do on close
+		setTitle("Comic Reader");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		//Set the size of the window to the size of the screen using Toolkit from awt
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize(screenSize.width, screenSize.height);
+
+		//Calling selfmade function to show the homescreen
+		showHomescreen();
+
+
+
+
+
+	}
+
+	public void showHomescreen() {
+		Homescreen homescreenPanel = new Homescreen();
+		add(homescreenPanel);
+		setVisible(true);
 	}
 
 	public void showComic() {
