21322e012fcf9749195532f012fb15f6e548e653
sander van der werff <sander-werff@hotmail.com>
2024-10-15 16:18:57 +0200

    ADD first progress of the FileHandler class. Formatting filenames and first bit of importing the files

--- src/main/java/org/ComicReaderSander/Main.java
+++ src/main/java/org/ComicReaderSander/Main.java
@@ -1,10 +1,18 @@
 package org.ComicReaderSander;
 
+import org.ComicReaderSander.Model.FileHandler;
 import org.ComicReaderSander.View.SimpleComicUI;
+import org.ComicReaderSander.Model.ComicGIF;
+
 
 public class Main {
 
     public static void main(String[] args) {
+        ComicGIF comic = new ComicGIF();
+        //comic.parseComic();
+        //comic.extractZip();
+        FileHandler.importFile();
+        FileHandler.createMapForComic("X-Men 005 (2024) (digital) (Marika-Empire).cbz");
         SimpleComicUI UI = new SimpleComicUI();
         UI.setVisible(true);
     }
--- src/main/java/org/ComicReaderSander/View/Homescreen.java
+++ src/main/java/org/ComicReaderSander/View/Homescreen.java
@@ -10,7 +10,14 @@ public class Homescreen extends JPanel {
     private JPanel Homescreen;
 
     public Homescreen() {
-        setBackground(Color.BLACK);
+        // Initializing homescreen
+
+        setBackground(new Color(33,33,33,255));
+
+        // Sets grid layout, 0 rows means dynamic amount of rows
+        setLayout(new GridLayout(0, 8, 10, 10));
 
     }
+
+
 }
